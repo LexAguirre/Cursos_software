cec60ec480303e9ff72fccb44fb2fc51e323c935
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-05 13:18:16 -0600

    Implamented class 16 ciclo for

---
+++ Python-en-platzi/16-Ciclo_for.py
@@ -0,0 +1,11 @@
+# contador = 0;
+
+# while contador < 10:
+#     print(contador);
+#     contador += 1; #Atajo que equivale a poner el contador + 1
+    
+# for contador in range(1001): # toma el rango de numeros desde el 0 hasta el numero anterior al 1000
+#     print(contador);
+
+for i in range(10):
+    print(11 * i);
