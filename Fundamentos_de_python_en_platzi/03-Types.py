49db676c50f8848b06f88aa0076a4fadeecc39de
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-07 19:38:33 -0600

    Viewed class 3 Types

---
+++ Fundamentos_de_python_en_platzi/03-Types.py
@@ -0,0 +1,21 @@
+#Strings
+
+my_name = "Ramón";
+my_name = "Alex";
+print("my_name", my_name);
+print(type(my_name));
+
+#int
+my_age = 12
+print("my_age =>", my_age);
+print(type(my_age));
+
+#boolean
+is_single = False;
+print("is_single =>", is_single);
+print(type(is_single));
+
+#inputs
+my_age = input("¿Cual es tu edad?");
+print("my_age =>", my_age);
+print(type(my_age));
