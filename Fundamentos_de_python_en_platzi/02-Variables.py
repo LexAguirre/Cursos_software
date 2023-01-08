e0bad975489195c2da762df7153fbf959df39a83
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-07 19:33:07 -0600

    Viewed class 2 variables

---
+++ Fundamentos_de_python_en_platzi/02-Variables.py
@@ -0,0 +1,17 @@
+print("Hola, vars");
+
+#Esto es una variable con un string
+my_name = "Ramón";
+print(my_name);
+
+#Esto es una variable con un número
+my_age = 12;
+print(my_age);
+
+#Esta es la actualización de una variable
+my_name = "Alex";
+print("Aqui cambio", my_name);
+
+#Input 
+my_name = input("¿Cual es tu nombre? ");
+print("Usando input", my_name);
