e242af54e84eaee4252cc5cc8ddfb019e0b29dbd
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-07 18:48:53 -0600

    Viewed class 1 Actions

---
+++ Fundamentos_de_python_en_platzi/01-Acciones.py
@@ -0,0 +1,19 @@
+print("Hola, esto es python");
+
+print("Hola soy Ramón y tengo 20 años");
+
+#Operaciones
+
+print(12 + 5);
+print(10 - 5);
+print(2 * 3);
+print(8 / 2);
+
+#Esto es un comentario
+
+"""
+Comentario
+de 
+varias 
+lineas
+"""
