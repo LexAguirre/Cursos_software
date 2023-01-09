fc9a7d87bee62f30f11264d51da370c88cf91add
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-08 23:37:56 -0600

    Viewed class 5 numbers

---
+++ Fundamentos_de_python_en_platzi/05-Numbers.py
@@ -0,0 +1,26 @@
+lives = 3;
+print(type(lives));
+
+age = 20;
+buget = 100;
+temperature = 12.12;
+
+print(type(temperature));
+
+lives = 12 + 15;
+print(lives);
+
+lives = lives - 1;
+print(lives);
+
+lives -= 1;
+print(lives);
+
+lives -= 5;
+print(lives);
+
+lives += 5;
+print(lives);
+
+number_1 = 4500000000000000000000000000;
+print(number_1);
