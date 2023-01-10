44b6c2da85206c0b25d3d4c2ee2ef99d2a2975bc
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-09 18:26:04 -0600

    Viewed class 10 and 11 Operators

---
+++ Fundamentos_de_python_en_platzi/10-Arithmetic_operators.py
@@ -0,0 +1,14 @@
+print(10 + 10);
+print(10 - 5);
+print(10 * 2);
+print(10 / 2);
+print(10 % 2);
+print(10 // 2);
+print(2 ** 3);
+print(2 ** 3 + 3 - 7 / 1 // 4);
+print(2 ** 3);
+print((7 / 1) // 4);
+print(8 + 3 - 1);
+
+print("Hola" + " mundo");
+print("Hola" * 3);
---
+++ Fundamentos_de_python_en_platzi/11-Comparison_operators.py
@@ -0,0 +1,34 @@
+# >
+print(7 > 3);
+print(3 > 7);
+print(7 > 7);
+
+# <
+print(5 < 6);
+print(6 < 5);
+print(6 < 6);
+
+# >=
+print(2 >= 1);
+print(2 >= 3);
+print(2 >= 2);
+
+# <=
+print(2 <= 1);
+print(1 <= 2);
+print(2 <= 2);
+
+# ==
+print(2 == 2);
+print(5 == 2);
+
+# !=
+print(6 != 10);
+print(6 != 6);
+print("Apple" == 'Apple');
+print("Apple" == 'apple');
+print("1" == 1);
+
+age = 18;
+print(age == 18);
+print(age == 19);
