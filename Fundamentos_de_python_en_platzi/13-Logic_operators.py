e32b7645046adbc520348b40fe598d709a808450
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-09 19:06:19 -0600

    Viewed class 13 Logic

---
+++ Fundamentos_de_python_en_platzi/13-Logic_operators.py
@@ -0,0 +1,24 @@
+#and 
+print("AND");
+print("True and True =>", True and True);
+print("False and True =>", False and True);
+print("True and False =>", True and False);
+print("False and False =>", False and False);
+
+print(10 > 5 and 5 < 10);
+print(10 > 5 and 5 > 10);
+
+stock = input("Input the number of the stock");
+stock = int(stock);
+
+print(stock >= 100 and stock <= 1000);
+
+print("OR");
+print("True or True =>", True or True);
+print("False or True =>", False or True);
+print("True or False =>", True or False);
+print("False or False =>", False or False);
+
+role = input("Digita el rol => ");
+
+print(role == "admin" or role == "seller");
