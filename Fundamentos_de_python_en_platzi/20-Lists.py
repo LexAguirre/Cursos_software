85efcdafac148fcfa13d7e64faf434ee2d2c2792
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-10 21:04:13 -0600

    Viewed class 20 Lists

---
+++ Fundamentos_de_python_en_platzi/20-Lists.py
@@ -0,0 +1,26 @@
+numbers = [1, 2, 3, 4];
+print(numbers);
+print(type(numbers));
+
+tasks = ["Make the dishes", "Play videogames"];
+print(tasks);
+
+types = [1, True, "Hello"];
+print(types);
+
+print(numbers[0]);
+print(tasks[0]);
+
+text = "Hello";
+
+tasks[0] = "Watch a course at platzi";
+print(tasks);
+
+tasks[0] = "Do the dishes";
+print(tasks);
+
+print(numbers[:3]);
+print(True in types);
+print("Hello" in types);
+print("Change" in types);
+
