5d82c0bc9971e9d814e16f74b5d78a481a9f48a7
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-14 23:52:25 -0600

    Viewed class 05 List cpmprehention

---
+++ Python_Comprehensions_Funciones_y_Errores/05-List_comprehention.py
@@ -0,0 +1,22 @@
+"""
+numbers = [];
+for element in range(1, 11):
+    numbers.append(element);
+
+print(numbers);
+
+numbers_v2 = [element for element in range(1, 11)];
+print(numbers_v2);
+
+numbers_v3 = [element * 2 for element in range(1, 11)];
+print(numbers_v3);
+"""
+
+numbers = [];
+for i in range(1, 11):
+    if i % 2 == 0:
+        numbers.append(i * 2);
+print(numbers);
+
+numbers_v2 = [i * 2 for i in range(1, 11) if i % 2 == 0];
+print(numbers_v2);
