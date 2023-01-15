c1e6d4ffe5e10b5cc28508a0842aad18a8aad175
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-15 00:04:14 -0600

    Viewed class 06 dictionary comprehention

---
+++ Python_Comprehensions_Funciones_y_Errores/06-Dictionary_comprehention.py
@@ -0,0 +1,37 @@
+import random
+
+def run():
+    """
+    dict = {};
+
+    for i in range(1, 11):
+        dict[i] = i * 2;
+
+    print(dict);
+
+    dict_v2 = {i: i * 2 for i in range(1, 5)}
+    print(dict_v2);
+    """
+    """
+    countries = ["col", "mex", "bol", "pe"];
+    population = {};
+
+    for country in countries:
+        population[country] = random.randint(1, 100);
+        print(population);
+
+    population_v2 = {country: random.randint(1, 100) for country in countries};
+    print(population_v2);
+    """
+    names = ["nico", "zule", "santi"];
+    ages = [12, 56, 98];
+
+    print(list(zip(names, ages)));
+
+    new_dict = {name: age for (name, age) in zip(names, ages)};
+    print(new_dict);
+
+
+
+if __name__ == '__main__':
+    run();
