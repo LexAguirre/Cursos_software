b324b4bf434052544af21aaa2e173f455e24c2c2
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-14 23:44:37 -0600

    Implemented Playground test 1

---
+++ Python_Comprehensions_Funciones_y_Errores/04-Playground_test_1.py
@@ -0,0 +1,48 @@
+"""
+Para resolver este desafío, debes escribir un algoritmo que elimine los elementos repetidos para obtener un conjunto único llamado new_set.
+
+Este algoritmo recibirá como entrada cuatro conjuntos de países, estos países serán de todo el continente americano divididos de la siguiente manera:
+
+countries - Países del continente en general.
+northAmerica - Países del norte de América.
+centralAmerica - Países del centro de América.
+southAmerica - Países del sur de América.
+En resumen, el algoritmo deberá eliminar los elementos repetidos de los cuatro conjuntos de países y obtener un conjunto único llamado new_set.
+
+Ejemplo 1:
+
+Input: 
+{"MX", "COL", "ARG", "USA"},
+{"USA", "CA"},
+{"MX", "GT", "BZ"},
+{"COL", "BZ", "ARG"}
+
+Output:
+{'ARG', 'USA', 'CANADA', 'GT', 'COL', 'MX', 'BZ'}
+
+Ejemplo 2:
+
+Input:
+{"BOL"},
+{"CA"},
+{"MX"},
+{"COL"}
+
+Output:
+
+{'COL', 'CA', 'BOL', 'MX'}
+"""
+
+def run():
+    countries = {"MX", "COL", "ARG", "USA"}
+    northAm = {"USA", "CANADA"}
+    centralAm = {"MX", "GT", "BZ"}
+    southAm = {"COL", "BZ", "ARG"}
+
+    # Escribe tu solución 👇
+    new_set = set(countries | northAm | centralAm | southAm);
+    
+    print(new_set);
+
+if __name__ == "__main__":
+    run();
