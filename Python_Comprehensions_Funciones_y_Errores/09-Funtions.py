b9de2dc168636ed6b36b01cd1513c1b86e08dc4a
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-15 22:48:13 -0600

    Viewed class 09 Functions

--- Python_Comprehensions_Funciones_y_Errores/09-Funtions.py
+++ Python_Comprehensions_Funciones_y_Errores/09-Funtions.py
@@ -1,5 +1,15 @@
 def run():
-    
+    def my_print(text):
+        print(text * 2);
+
+    my_print("This is my print ");
+    my_print("Hello there! ");
+
+    def suma(a, b):
+        my_print(a + b);
+
+    suma(1, 5);
+    suma(10, 15);
 
 
 if __name__ == '__main__':
