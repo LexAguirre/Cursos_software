5a5279b12d5197ec674f33cab20209234a773a9a
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-01-27 17:13:07 -0600

    Viewed class 30 Errors

---
+++ Python_Comprehensions_Funciones_y_Errores/30-errors.py
@@ -0,0 +1,15 @@
+# print(0/0); #Divicion en cero causa error
+# print(result); #No se encuentra la variable a ejecutar
+print("hola")
+
+# suma = lambda x,y: x + (y * 2);
+# assert suma(2, 2) == 4; #Causa un assertionError ya que la funcion no es correcta
+
+print("hola 2")
+
+age = 10
+if age < 18:
+    # Lanza un error pero que tu lo programaste
+    raise Exception("You aren't an adult")
+
+print("hola 3")
