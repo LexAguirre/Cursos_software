03c3aa71d3f132c601f411f8a36bd518cdaebd33
LexAguirre <ramonlexaguirre@gmail.com>
2023-12-18 11:20:44 -0600

    Started python backend clases

---
+++ Python_backend/01_turtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+windows = turtle.Screen()   
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+
+
+windows.mainloop()
